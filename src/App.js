30d43c9c1e146b416440104aca5a7b53fc92489a
hinkleydev <azz@hinkley.dev>
2024-11-25 15:51:46 +0000

    ADD(frontend): Basic file format

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Block from './components/Block';
 
 function App() {
+  /**
+   * Data should come in the format of an object containing
+   * title - a string that shows the basic use of the data
+   * metadata - an object of key-values pairs for information like status codes and headers
+   * content - an object containing the response content to display to the user
+   */
+  const [data, setData] = useState([ {
+    id: 1,
+    title: "This is a piece of data",
+    metadata: { "status" : 200 },
+    content: {"response_time" : "33ms"}
+  } ]);
   return (
     <div className="App">
       <header className="App-header">
-        <h2>OpenMirage</h2>
+        <h1>OpenMirage</h1>
       </header>
+      <div>
+        {data.map(function(item) {
+          return <Block id={item.key} title={item.title} metadata={item.metadata} content={item.content} />
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/Block.js
@@ -0,0 +1,7 @@
+function Block({title, metadata, content}) {
+    return (<div>
+        <h3>{title}</h3>
+    </div>);
+}
+
+export default Block;
