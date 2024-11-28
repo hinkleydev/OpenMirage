b3af2a2e94cc254d59968d8c1c42db49b2b3e32b
hinkleydev <azz@hinkley.dev>
2024-11-28 11:33:55 +0000

    ADD(API): Basic server routes

--- server/routes/requests.js
+++ server/routes/requests.js
@@ -1,9 +1,34 @@
 const express = require('express')
 const router = express.Router()
+const requests = require('../requests.json');
+// The best course of action might be using an external file to get this info
 
-// GET /requests
-router.get('/', (req, res) => {
-    res.send("Working!")
+// --- CREATE
+
+router.post('/:request', (req, res) => {
+    // Code
 })
+
+// --- READ
+
+/**
+ * GET /requests
+ * Return a list of all possible requests to make
+ */
+router.get('/', (req, res) => {
+    // Code
+})
+
+/**
+ * GET /requests/:request
+ * Return details of a specific request and how to make it
+ */
+router.get('/:request', (req, res) => {
+    // Code
+})
+
+// --- UPDATE
+
+// --- DELETE
 
 module.exports = router
