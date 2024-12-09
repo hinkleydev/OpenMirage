d41c8f7c9899ab4cb12b13c7f105ad4530d99425
hinkleydev <azz@hinkley.dev>
2024-12-09 13:08:28 +0000

    ADD(frontend): Implement adjustable view size for Block component

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react";
+
 function Block({title, metadata, content}) {
     let classes = content == "Loading..." ? "loading card" : "card";
-    return (<div className={classes}>
+    const [viewMode, setViewMode] = useState(1); // Set the view size
+    useEffect(function() {
+        if(viewMode < 1) {
+            setViewMode(1); // Safety barrier so it can't be shrunk to nothing
+        }
+    }, [viewMode])
+
+    return (<div className={classes} style={{width: (20 * viewMode) + "%"}}>
         <h3>
             {title}
         </h3>
         <div className="result-content">
             {content}
             {content == "Loading..." ? <div className="loading-symbol"></div> : null}
+        </div>
+        <div className="viewsize">
+            <button onClick={() => setViewMode(viewMode + 1)}>+</button> {/* Enlarge view */}
+            <button onClick={() => setViewMode(viewMode - 1)}>-</button> {/* Shrink view */}
+            <button onClick={() => setViewMode(4)}>&lt;-&gt;</button> {/* Full view */}
         </div>
     </div>);
 }
