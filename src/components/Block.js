bab9e32844f51d410244da5cc550c52e57b8bfd0
hinkleydev <azz@hinkley.dev>
2024-12-10 13:18:29 +0000

    FEAT(frontend): Remake as grid layout

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,29 +1,31 @@
 import { useEffect, useState } from "react";
 
-function Block({title, deleteCard, content, id}) {
-    let classes = content == "Loading..." ? "loading card" : "card";
+function Block({ title, deleteCard, content, id }) {
+    let classes = content === "Loading..." ? "loading card" : "card";
     const [viewMode, setViewMode] = useState(1); // Set the view size
-    useEffect(function() {
-        if(viewMode < 1) {
+
+    useEffect(() => {
+        if (viewMode < 1) {
             setViewMode(1); // Safety barrier so it can't be shrunk to nothing
         }
-    }, [viewMode])
+    }, [viewMode]);
 
-    return (<div id={id} className={classes} style={{width: (20 * viewMode) + "%"}} tabIndex="-0">
-        <h3>
-            {title}
-        </h3>
-        <div className="result-content">
-            {content}
-            {content == "Loading..." ? <div className="loading-symbol"></div> : null}
+    return (
+        <div id={id} className={classes} style={{ gridColumn: "span " + viewMode }} tabIndex="-0">
+            <h3 title={title}>
+                {title}
+            </h3>
+            <div className="result-content">
+                {content}
+                {content === "Loading..." ? <div className="loading-symbol"></div> : null}
+            </div>
+            <div className="card-buttons">
+                <button onClick={deleteCard}>X</button> {/* Delete card */}
+                <button onClick={() => setViewMode(viewMode - 1)}>-</button> {/* Shrink view */}
+                <button onClick={() => setViewMode(viewMode + 1)}>+</button> {/* Enlarge view */}
+            </div>
         </div>
-        <div className="card-buttons">
-            <button onClick={deleteCard}>X</button> {/* Delete card */}
-            <button onClick={() => setViewMode(viewMode - 1)}>-</button> {/* Shrink view */}
-            <button onClick={() => setViewMode(viewMode + 1)}>+</button> {/* Enlarge view */}
-            <button onClick={() => setViewMode(4)}>&lt;-&gt;</button> {/* Full view */}
-        </div>
-    </div>);
+    );
 }
 
 export default Block;
